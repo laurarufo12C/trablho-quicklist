411e7a84f64f13c5cd818c469331e0deb279f838
Laura Rufo <laurinharufo08@gmail.com>
2025-06-01 21:39:39 -0300

    criação da função showitemslist para mostrar na tela os itens

--- main.js
+++ main.js
@@ -4,11 +4,37 @@ function addItem() {
     const itemName = document.querySelector("#item").value
 
     const item = {
-        naame: itemName,
+        name: itemName,
         checked: false
     }
 
     itens.push(item)
 
     document.querySelector("#item").value = ""
+
+    showItensList()
+}
+
+function showItensList(){
+    const sectionList = document.querySelector(".list")
+    sectionList.textContent = ""
+
+    itens.map((item, index) => {
+        sectionList.innerHTML += `
+            <div class="item">
+                <div>
+                    <input type="checkbox" name="list" id="item-${index}">
+
+                    <div class="custom-checkbox">
+                        <img src="./assets-20250601T213559Z-1-001/assets/checked.svg" alt="checked">
+                    </div>
+                    <label for="item-${index}">${item.name}</label>
+                </div>
+
+                <button>
+                    <img src="./assets-20250601T213559Z-1-001/assets/trash-icon.svg" alt="trash icon">
+                </button>
+            </div>
+        `
+    })
 }
